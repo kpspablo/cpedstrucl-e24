0194bf06c9391a2aae27b4d91582319eecd56a90
Karla Patricia Pablo <kpspablo@users.noreply.github.com>
2017-02-17 20:31:38 -0800

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,8 +1,8 @@
 #include<iostream> 
 using namespace std;
 int main(){
-	int count(0),j;
-  	int a[]= {35,10,55,20,5};     
+    int count(0),j;
+    int a[]= {35,10,55,20,5};     
     for(int i=1;i<5;++i){
         for(int j=0;j<(5-i);++j){
             if(a[j]>a[j+1]){
@@ -12,10 +12,10 @@ int main(){
             }
         cout << "PASS: " <<++count<<endl; 
         for(int k=0;k<5;++k){
-        	cout<<a[k]<<" ";  
+           cout<<a[k]<<" ";  
     	}
-		cout << "SWAPPED: "<<a[j]<<" "<<a[j+1]<<endl;
+	cout << "SWAPPED: "<<a[j]<<" "<<a[j+1]<<endl;
+        }
     }
-}
     return 0;
 }
