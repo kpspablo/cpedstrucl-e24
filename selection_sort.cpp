cc21483d0bd5e461c03b8408e20011bedc245c7f
Karla Patricia Pablo <kpspablo@users.noreply.github.com>
2017-02-17 20:44:28 -0800

    Update selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -5,16 +5,16 @@ int main(){
     int a[]= {30, 40, 20, 50, 10};     
     for(int i=0;i<5-1;i++){
         int min=a[i], index=i;
-		for(int j=i+1;j<5;j++)
+	for(int j=i+1;j<5;j++)
             if(a[j]<min){
                 min = a[j];
                 index=j;
             }
-        a[index] =a[i];
-        a[i]=min;
-	cout << "PASS " <<++count<<endl;  
-        for(int k=0;k<5;++k)
-        cout<<a[k]<<endl;   
+            a[index] =a[i];
+            a[i]=min;
+	    cout << "PASS " <<++count<<endl;  
+            for(int k=0;k<5;++k)
+               cout<<a[k]<<endl;   
     }
     return 0;
 }
