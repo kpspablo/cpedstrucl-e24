f7f0cf95f9f393dfb74b2a1a6a773e7a9cebb376
Karla Patricia Pablo <kpspablo@users.noreply.github.com>
2017-02-17 20:41:47 -0800

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -3,17 +3,15 @@ using namespace std;
 int main(){
 	int j, count(0);
   	int a[]= {40, 30, 20, 50, 10};     
-    for(int i=1;i<5;i++){
-        int tmp=a[i];
-		for(j=i-1;j>=0 && tmp<a[j];j--){
-			a[j+1]=a[j];
-		}
-		a[j+1]=tmp;
-	cout << "PASS " <<++count<<endl;     
-        for(int k=0;k<5;++k)
-        cout<<a[k]<<endl;   
-    }
-    return 0;
+        for(int i=1;i<5;i++){
+           int tmp=a[i];
+           for(j=i-1;j>=0 && tmp<a[j];j--){
+	      a[j+1]=a[j];
+           }
+	   a[j+1]=tmp;
+	   cout << "PASS " <<++count<<endl;     
+           for(int k=0;k<5;++k)
+              cout<<a[k]<<endl;   
+        }
+        return 0;
 }
-
-
