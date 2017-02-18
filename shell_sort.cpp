57592cad0d3f88a6f630190b0082580073782167
Karla Patricia Pablo <kpspablo@users.noreply.github.com>
2017-02-17 19:59:14 -0800

    Create shell_sort.cpp

---
+++ shell_sort.cpp
@@ -0,0 +1,24 @@
+#include<iostream> 
+using namespace std;
+void shell_sort(int *a, int n){
+	int count(0);
+	int h, i, j, k;
+	for(h=n; h/=2;){
+		for(i=h; i<n; i++){
+			k=a[i];
+			for(j=i; j>=h && k<a[j-h]; j-=h){
+				a[j] = a[j-h];
+			}
+			a[j]=k;
+			cout << "PASS: " <<++count<<endl;  
+        	for(int k=0;k<5;++k)
+        		cout<<a[k]<<endl;     
+		}
+	}
+}
+int main(int ac, char **av){
+    int a[]= {40, 30, 20, 50, 10}; 
+	int n = 5;
+	shell_sort(a,n);  
+    return 0;
+}
